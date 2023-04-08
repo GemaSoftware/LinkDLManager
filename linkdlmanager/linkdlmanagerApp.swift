da3979349128ab1e207401fee9ffd33d3405fa73
GemaSoftware <41391455+GemaSoftware@users.noreply.github.com>
2023-04-08 09:34:41 -0400

    Initial Commit

---
+++ linkdlmanager/linkdlmanagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  linkdlmanagerApp.swift
+//  linkdlmanager
+//
+//  Created by Agron Gemajli on 4/8/23.
+//
+
+import SwiftUI
+
+@main
+struct linkdlmanagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
